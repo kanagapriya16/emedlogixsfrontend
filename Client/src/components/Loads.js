6b69d22847bcd24916adb87d35b8589ffc84286c
krishna1621 <115209389+krishna1621@users.noreply.github.com>
2023-08-21 15:27:10 +0530

    new changes client

--- Client/src/components/Loads.js
+++ Client/src/components/Loads.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 import"../styles/Loads.css"
@@ -10,17 +9,5 @@ export const Loads = () => {
       <div class="bounce3"></div>
     </div>
   );
-=======
-import React from "react";
 
-import"../styles/Loads.css"
-export const Loads = () => {
-  return (
-    <div class="spinner">
-      <div class="bounce1"></div>
-      <div class="bounce2"></div>
-      <div class="bounce3"></div>
-    </div>
-  );
->>>>>>> bee7b9dd30db30548833e98ac68433c1269cd860
-};
+  }
