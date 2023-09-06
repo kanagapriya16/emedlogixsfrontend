05acc2fb331f505005b8db5210250679b695c8e5
kanagapriya16 <135326850+kanagapriya16@users.noreply.github.com>
2023-09-06 10:33:00 +0530

    code new

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM node:18-alpine
 WORKDIR /claritui
 ENV PATH="/claritui/node_modules/.bin:$PATH"
 COPY package.json ./
-COPY package.json ./
+COPY package-lock.json ./
+RUN npm install
 COPY  . ./
 RUN npm run build
 EXPOSE 3000
--- src/components/sign-Up.js
+++ src/components/sign-Up.js
@@ -12,8 +12,9 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import axios from "axios";
+
 import { Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Copyright(props) {
   return (
